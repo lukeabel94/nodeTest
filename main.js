eea1a492c0945cedef0c93598943fab552b51aba
Luke Abel <label017@hotmail.com>
2017-05-13 21:35:11 +1000

    added HTTP server

---
+++ main.js
@@ -0,0 +1,13 @@
+var http = require("http");
+http.createServer(function(request, response){
+  // Send HTTP header
+  // HTTP status : 200: OK
+  // Consent Type: text/plain
+  response.writeHead(200, {'Content-Type' : 'text/plain'});
+
+  // Send the response body as "Hello World"
+  response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log("Server running at http://127.0.0.1:8081/");
